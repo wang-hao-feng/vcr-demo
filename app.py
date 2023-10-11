5d661828ed85305b2eed52e2d4d9060dda9c9156
wang-hao-feng <919576020@qq.com>
2023-10-12 00:00:38 +0800

    finish framwork

--- app.py
+++ app.py
@@ -2,21 +2,37 @@ import gradio as gr
 
 vcr_dataset = [None] * 10
 
-with gr.Blocks() as demo:
+#css = "#button0 {--button-secondary-background-fill: rgba(255, 0, 0, 0.78); --button-secondary-background-fill-hover: rgba(255, 0, 0, 0.5);}"
+button_css = [f"button{i}" + "{--button-secondary-background-fill: linear-gradient(to bottom right, var(--neutral-600), var(--neutral-700));\
+               --button-secondary-background-fill-hover: linear-gradient(to bottom right, var(--neutral-600), var(--neutral-600));}" for i in range(64)]
+css = "\n".join(button_css)
+
+with gr.Blocks(css=css) as demo:
     with gr.Row():
         with gr.Column():
             index = gr.Slider(maximum=len(vcr_dataset), step=1, label='index')
-            image = gr.Image()
+            random_idx = gr.Button('Random')
+            image_board = gr.Image()
+            buttons = {}
+            for j in range(8):
+                with gr.Row():
+                    for i in range(8):
+                        buttons[i+j*8] = gr.Button(f'button {i+j*8}', size='sm', min_width=64, elem_id=f'button{i+j*8}')
+            """ with gr.Row():
+                for i in range(8):
+                    with gr.Column():
+                        for j in range(8):
+                            buttons[i + j * 8] = gr.Button(f'button {i + j * 8}', size='sm') """
             #buttons = [gr.Button(value=f'person {i}', min_width=20) for i in range(2)]
         with gr.Column():
             question = gr.HighlightedText(label='question', 
-                                          value=[('question', 'False')], 
+                                          value=[('question', None)], 
                                           color_map={'False':'white'})
-            answer_choices = gr.HighlightedText(label='answer_choices', 
-                                               value=[('a\n', 'True'), ('b\n', 'False'), ('c\n', 'False'), ('d\n', 'False')], 
-                                               color_map={'True':'green', 'False':'white'})
-            rationale_choices = gr.HighlightedText(label='rationale_choices', 
-                                                   value=[('a\n', 'True'), ('b\n', 'False'), ('c\n', 'False'), ('d\n', 'False')], 
-                                                   color_map={'True':'green', 'False':'white'})
+            answer_choices = gr.HighlightedText(label='answer choices', 
+                                               value=[('a\n', 'True'), ('b\n', None), ('c\n', None), ('d\n', None)], 
+                                               color_map={'True':'green'})
+            rationale_choices = gr.HighlightedText(label='rationale choices', 
+                                                   value=[('a\n', 'True'), ('b\n', None), ('c\n', None), ('d\n', None)], 
+                                                   color_map={'True':'green'})
 
 demo.launch()
--- dataset.py
+++ dataset.py
@@ -3,37 +3,39 @@ from PIL import Image, ImageDraw, ImageFont
 import os
 import jsonlines
 import json
+from tqdm import tqdm
 
 from torch.utils.data import Dataset
 
 class VCRDataset(Dataset):
     def __init__(self, path, split='train'):
         assert split in ['train', 'val']
-        self.image_root = os.path.join(path, 'vcr_images/vcr1image')
-        with jsonlines.open(os.path.join(path, f'vcr_annotation/{split}_prepare.jsonl'), 'r') as reader:
+        self.image_root = os.path.join(path, 'vcr_images/vcr1images')
+        with jsonlines.open(os.path.join(path, f'vcr_annotation/{split}.jsonl'), 'r') as reader:
             self.datas = [self.__parse(data) for data in reader.iter()]
     
     def __parse(self, data:dict):
         #image
         objects = data['objects']
-        image_path = os.path.join(self.image_root, data['img_fn'])
-        metadata_path = os.path.join(self.image_root, data['metadata_fn'])
-        with open(metadata_path, 'r') as f:
+        image_path = os.path.join(self.image_root, data['img_fn'].replace('...', ''))
+        metadata_path = os.path.join(self.image_root, data['metadata_fn'].replace('...', ''))
+        """ with open(metadata_path, 'r') as f:
             metadata = json.load(f)
         bboxes = [bbox[:-1] for bbox in metadata['boxes']]
-        segms = [[(x, y) for x, y in segm] if segm is not None else [] for segm in data['segms']]
+        segms = [[(x, y) for x, y in segm[0]] if len(segm) > 0 else [] for segm in metadata['segms']] """
         
         #test
         question = data['question_orig']
         answer_choices = [' '.join([word if isinstance(word, str) else str(word)[1:-1] for word in choice]) for choice in data['answer_choices']]
-        answer_label = data['asnwer_label']
+        answer_label = data['answer_label']
         rationale_choices = [' '.join([word if isinstance(word, str) else str(word)[1:-1] for word in choice]) for choice in data['rationale_choices']]
         rationale_label = data['rationale_label']
         
         return {'objects': objects, 
                 'image_path': image_path, 
-                'bboxes': bboxes, 
-                'semgs': segms, 
+                #'bboxes': bboxes, 
+                #'semgs': segms, 
+                'metadata_path': metadata_path, 
                 'question': question, 
                 'answer_choices': answer_choices, 
                 'answer_label': answer_label, 
@@ -43,10 +45,14 @@ class VCRDataset(Dataset):
     def __getitem__(self, index) -> Any:
         data = self.datas[index]
         image = Image.open(data['image_path'])
+        with open(data['metadata_path'], 'r') as f:
+            metadata = json.load(f)
+        bboxes = [bbox[:-1] for bbox in metadata['boxes']]
+        segms = [[(x, y) for x, y in segm[0]] if len(segm) > 0 else [] for segm in metadata['segms']]
         return {'objects': data['objects'], 
                 'image': image, 
-                'bboxes': data['bboxes'], 
-                'semgs': data['segms'], 
+                'bboxes': bboxes, 
+                'segms': segms, 
                 'question': data['question'], 
                 'answer_choices': data['answer_choices'], 
                 'answer_label': data['answer_label'], 
@@ -59,27 +65,28 @@ class VCRDataset(Dataset):
 if __name__ == '__main__':
     vcr = None
     # read test
-    from tqdm import tqdm
-    for split in ['train', 'val']:
-        vcr = VCRDataset('', split=split)
-        for data in tqdm(vcr, desc=split):
-            pass
+    vcr = VCRDataset('G:/vcr', split='val')
+    max_num = 0
+    for data in tqdm(vcr):
+        max_num = max(max_num, len(data['objects']))
+    print(max_num)
     
     #draw test
     data = vcr[0]
     image_orig, objects, bboxes, segms = data['image'], data['objects'], data['bboxes'], data['segms']
     image = image_orig.copy()
     draw = ImageDraw.Draw(image, 'RGBA')
-    font = ImageFont('arial.ttf', 20)
+    font = ImageFont.truetype('arial.ttf', 20)
     import random
-    colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(objects))]
+    colors = [tuple([random.randint(0, 255) for _ in range(3)]) for _ in range(len(objects))]
     for idx, o in enumerate(objects):
-        draw.rectangle(bboxes[idx], outline=colors[idx], width=2)
-        draw.polygon(segms[idx], outline=colors[idx], width=2) if len(segms[idx] > 0) else None
+        draw.rectangle(bboxes[idx], outline=colors[idx]+(200, ), width=3)
+        draw.polygon(segms[idx], outline=colors[idx]+(200, ), width=3) if len(segms[idx]) > 0 else None
         text = f'{o} {idx}'
-        text_bbox = draw.textbbox((0, 0), text, font, align='center')
+        w, h = bboxes[idx][:2]
+        text_bbox = draw.textbbox((w, h), text, font, align='center', spacing=2)
         text_w, text_h = text_bbox[2] - text_bbox[0], text_bbox[3] - text_bbox[1]
-        w, h = image.size
-        draw.text((w - text_w, h - text_h), text, font=font)
+        draw.rectangle(text_bbox, fill=colors[idx]+(200, ))
+        draw.text((w + 3, h), text, font=font, fill=(0, 0, 0), align='center', spacing=2)
     image.show()
         
